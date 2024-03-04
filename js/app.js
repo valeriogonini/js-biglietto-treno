7e4a1055c7cec0cc810e673c7f33684430f8d7f0
valeriogonini <valeriogonini@gmail.com>
2024-03-04 17:42:53 +0100

    delete parsfloat

--- js/app.js
+++ js/app.js
@@ -1,46 +1,44 @@
 //chilometri del passeggero 
 const km = 500 //numb
 
-console.log("chilometri passeggero",km, "km");
+console.log("chilometri passeggero", km, "km");
 
 
 
 
 
 //prezzo del biglietto in base ai km (0.21km)
-const prezzo = (km * 0.21); 
+const prezzo = (km * 0.21); //numb
 
-console.log("prezzo standard",prezzo, "euro");
+console.log("prezzo standard", prezzo, "euro");
+
+//sconto dei minorenni 20% sconto 40% per over 65
+
 
 //sconto dei minorenni 20%
+let prezzosconto20 = (prezzo * 0.2); //numb
+let prezzoscontato20 = prezzo - prezzosconto20;
+
+console.log("sconto 20% minorenni", prezzoscontato20, "euro");
 //sconto 40% per over 65
+let prezzosconto40 = (prezzo * 0.4);//numb
+let prezzoscontato40 = prezzo - prezzosconto40;
 
-//sconto dei minorenni 20%
-let prezzominor20 = parseFloat(prezzo * 0.2)
-let prezzoscontato20 = prezzo - prezzominor20 ;
-
-console.log("sconto 20% minorenni",prezzoscontato20, "euro");
-//sconto 40% per over 65
-let prezzominor40 = parseFloat(prezzo * 0.4)
-let prezzoscontato40 = prezzo - prezzominor40 ;
-
-console.log("sconto 40% over 65",prezzoscontato40, "euro");
+console.log("sconto 40% over 65", prezzoscontato40, "euro");
 
 //età passeggero
-const eta = prompt("Inserisci l'età")
-console.log("la tua età:" ,eta);
+const etaAsStr = prompt("Inserisci l'età") //string
+const etaAsNumber = parseInt(etaAsStr); // numb
+console.log("la tua età:", etaAsNumber); //numb
 
-if (eta >=0 && eta<=18)
-{
-    console.log("Prezzo minori 18 anni:" ,prezzoscontato20, "euro");
+if (etaAsNumber >= 0 && etaAsNumber <= 18) {
+    console.log("Prezzo minori 18 anni:", prezzoscontato20, "euro");
 
-} else if (eta > 18 && eta < 65)
-{
-console.log("Prezzo standard:",prezzo, "euro");
+} else if (etaAsNumber > 18 && etaAsNumber < 65) {
+    console.log("Prezzo standard:", prezzo, "euro");
 
-} else(eta >= 65)
-{
-console.log( "Prezzo over 65:" ,prezzoscontato40, "euro");
+} else {
+    console.log("Prezzo over 65:", prezzoscontato40, "euro");
 
 }
 
